25502bd3743e532275fdab1a4ee012bc86a71021
Thanh Hoang <hoangpltde180633@fpt.edu.vn>
2024-01-26 08:01:12 +0700

    update geographic

--- GeographicManagement/src/view/CountryView.java
+++ GeographicManagement/src/view/CountryView.java
@@ -14,38 +14,60 @@ import utils.Inputter;
  * @author hoang
  */
 public class CountryView {
-    
+
     Inputter inputter = new Inputter();
-    
-    public void addCountryInfomation(ArrayList<EastAsiaCountries> arr){
-        EastAsiaCountries eACountry = new EastAsiaCountries();
-        eACountry.setCountryCode(inputter.getString("Enter code of country"));
-        eACountry.setCountryName(inputter.getString("Enter name of country"));
-        boolean flag = true;
-        while(flag){
-            eACountry.setTotalArea(inputter.getInputFloat("Enter total Area"));
-            if (eACountry.getTotalArea() > 0) flag = false;
+
+    public void addCountryInfomation(ArrayList<EastAsiaCountries> arr) {
+        String countryCode = "",countryName ="", countryTerrain = "";
+        float totalArea = 0;       
+        int eAsize = arr.size();
+        if (eAsize > 11) {
+            System.out.println("East Asia just have 11 country");
+        } else {
+            while(true){
+                countryCode = (inputter.getString("Enter code of country"));
+                if (!checkIfExistCode(arr, countryCode)) break;
+            }            
+            countryName = (inputter.getString("Enter name of country"));
+            boolean flag = true;
+            while (flag) {
+                totalArea = (inputter.getInputFloat("Enter total Area"));
+                if (totalArea > 0) {
+                    flag = false;
+                }
+            }
+            countryTerrain = (inputter.getString("Enter terrain of country"));
+            EastAsiaCountries eACountry = new EastAsiaCountries(countryTerrain, countryCode, countryName, totalArea);
+            arr.add(eACountry);
         }
-        eACountry.setCountryTerrain(inputter.getString("Enter terrain of country"));
-        arr.add(eACountry);
     }
-    
-    public void getRecentlyEnterdInfomation(ArrayList<EastAsiaCountries> arr) {  
-        System.out.printf("%-10s%-25s%-20s%-25s\n", "ID", "Name", "Total Area","Terrain");
+
+    public void getRecentlyEnterdInfomation(ArrayList<EastAsiaCountries> arr) {
+        System.out.printf("%-10s%-25s%-20s%-25s\n", "ID", "Name", "Total Area", "Terrain");
         arr.get(arr.size() - 1).display();
     }
-    
-    public void searchInfomationByName(ArrayList<EastAsiaCountries> arr){        
+
+    public void searchInfomationByName(ArrayList<EastAsiaCountries> arr) {
         String countryName = inputter.getString("Enter the name you want to search for");
-        System.out.printf("%-10s%-25s%-20s%-25s\n", "ID", "Name", "Total Area","Terrain");
+        System.out.printf("%-10s%-25s%-20s%-25s\n", "ID", "Name", "Total Area", "Terrain");
         for (EastAsiaCountries country : arr) {
             if (country.getCountryName().equalsIgnoreCase(countryName)) {
                 country.display();
             }
         }
     }
-    
-     public void printCountrySorted(ArrayList<EastAsiaCountries> lc) {
+
+    public boolean checkIfExistCode(ArrayList<EastAsiaCountries> arr, String code) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i).getCountryCode().equalsIgnoreCase(code)) {
+                System.out.println("Country code is exist");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printCountrySorted(ArrayList<EastAsiaCountries> lc) {
         Collections.sort(lc);
         System.out.printf("%-10s%-25s%-20s%-25s\n", "ID", "Name", "Total Area",
                 "Terrain");
